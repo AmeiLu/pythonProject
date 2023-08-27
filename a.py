a70d88bd378790fb02de7c63154f951502da8154
留着空白 <143216107+AmeiLu@users.noreply.github.com>
2023-08-27 16:44:13 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,12 +1,3 @@
 a = 2
 print(a)
 a = 3
-# git add 文件名。 2.追踪文件状态，查一下
-# git commit -m "feat: 注释" 3.添加到本地暂存区 
-# git
-# git status 1.查看状态
-# gitgit 
-# 设置自动保存，一般都是失去焦点就自动保存
-# git push 4.提交到GitHub
- 
- # 1.点击+号 暂存更改 2.消息框输入注释提交
